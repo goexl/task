e444382715e18777839c8ec28c2ffd1df1c7e038
storezhang <storezhang@gmail.com>
2024-08-16 20:36:15 +0800

    feat(处理器): 增加下次执行时间

--- processor.go
+++ processor.go
@@ -1,7 +1,20 @@
 package task
 
+import (
+	"time"
+)
+
 // Processor 处理器
 type Processor interface {
 	// Process 处理任务调度
 	Process(scheduling Scheduling) error
+
+	// Recyclable 是否继续执行
+	Recyclable() bool
+
+	// Next 下次执行时间
+	Next() time.Time
+
+	// Error 执行错误
+	Error() error
 }
