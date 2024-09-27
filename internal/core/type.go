5176e40e5c6b93e6a70ea84ec52dc0a837ddc0ae
storezhang <storezhang@gmail.com>
2024-09-27 20:06:22 +0800

    refactor(任务): 增加Type来标识任务

--- internal/core/type.go
+++ internal/core/type.go
@@ -1,5 +1,3 @@
 package core
 
-// Type 类型
-// 方便通过 Type 来定制化处理流程
 type Type uint16
---
+++ type.go
@@ -0,0 +1,9 @@
+package task
+
+import (
+	"github.com/goexl/task/internal/core"
+)
+
+// Type 类型
+// 方便通过 Type 来定制化处理流程
+type Type = core.Type
