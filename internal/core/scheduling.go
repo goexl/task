f1f322b6fb97fbe9a791154e8154c7e443ff0398
storezhang <storezhang@gmail.com>
2024-10-28 18:27:04 +0800

    refactor(项目): 暴露Scheduling

---
+++ internal/core/scheduling.go
@@ -0,0 +1,7 @@
+package core
+
+import (
+	"github.com/goexl/task/internal/internal/core"
+)
+
+type Scheduling = core.Scheduling
--- scheduing.go
+++ scheduing.go
@@ -2,8 +2,12 @@ package task
 
 import (
 	"github.com/goexl/task/internal/builder"
+	"github.com/goexl/task/internal/core"
 	"github.com/goexl/task/internal/kernel"
 )
+
+// Scheduling 计划
+type Scheduling = core.Scheduling
 
 func NewScheduling(target uint64, subtype kernel.Type) *builder.Scheduling {
 	return builder.NewScheduling(target, subtype)
